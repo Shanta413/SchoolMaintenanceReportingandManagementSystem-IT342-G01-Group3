35f0a76ff0d535c5a9835f0f4b8d332f7efb7cfb
Christian Jayson Cantiller <jaysoncan413@Gmail.com>
2025-11-12 16:54:09 +0800

    securityconfig

--- backend/smrms/src/main/java/com/smrms/smrms/security/SecurityConfig.java
+++ backend/smrms/src/main/java/com/smrms/smrms/security/SecurityConfig.java
@@ -111,7 +111,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+    "http://localhost:5173", // for local dev
+    "https://frontend-production-e168.up.railway.app" // for deployed site
+));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
