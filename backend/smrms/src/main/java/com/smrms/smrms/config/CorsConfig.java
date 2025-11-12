207dfecc7b3605ebcada467bf30987bd700882ea
Christian Jayson Cantiller <jaysoncan413@Gmail.com>
2025-11-12 16:46:51 +0800

    Cors

--- backend/smrms/src/main/java/com/smrms/smrms/config/CorsConfig.java
+++ backend/smrms/src/main/java/com/smrms/smrms/config/CorsConfig.java
@@ -1,4 +1,6 @@
-// src/main/java/com/smrms/smrms/config/CorsConfig.java
+package com.smrms.smrms.config;
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
