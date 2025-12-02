e1cf4c5d02d88264cce10b0a0e2bd4b70ceb7ce1
Christian Jayson Cantiller <jaysoncan413@Gmail.com>
2025-12-02 21:58:28 +0800

    feat(query): added react query on page BuildingSelection.jsx

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,13 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ThemeProvider } from "./context/ThemeContext.jsx";   // <-- ADD THIS
+// main.jsx / index.jsx
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider>                     {/* <-- WRAP HERE */}
-      <App />
-    </ThemeProvider>
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
